b3582e4a9c1fe6136e09ca3501bb48408fda9199
SejoMont <sejo.monterocastro@gmail.com>
2023-11-16 16:56:17 +0100

    crearPersonaje

--- src/app/components/nuevopersonaje/nuevopersonaje.component.ts
+++ src/app/components/nuevopersonaje/nuevopersonaje.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Series } from 'src/app/models/series';
 import { ServiceSeries } from 'src/app/services/service.series';
 import { Router } from '@angular/router';
+import { Personajes } from 'src/app/models/personajes';
 
 @Component({
   selector: 'app-nuevopersonaje',
@@ -16,7 +17,10 @@ export class NuevopersonajeComponent implements OnInit {
 
 
   public series!: Array<Series>;
-  constructor(private _serviceSeries: ServiceSeries) {}
+  constructor(
+    private _serviceSeries: ServiceSeries,
+    private _router: Router
+    ) {}
 
   ngOnInit(): void {
     this._serviceSeries.getSeries().subscribe((response) => {
@@ -24,14 +28,15 @@ export class NuevopersonajeComponent implements OnInit {
     });
   }
 
-  InsertarDepartamento(): void {
-    var num = parseInt(this.cajaIdRef.nativeElement.value);
+  CrearPersonaje(): void {
+    var id = 0
     var nom = this.cajaNombreRef.nativeElement.value;
-    var loc = this.cajaLocalidadRef.nativeElement.value;
+    var img = this.cajaImagenRef.nativeElement.value;
+    var ser = parseInt(this.cajaSerieRef.nativeElement.value);
 
-    var newDept = new Departamento(num, nom, loc);
+    var newPersonaje = new Personajes(id, nom, img, ser);
 
-    this._serviceDepartamentos.create(newDept).subscribe((response) => {
+    this._serviceSeries.addPersonaje(newPersonaje).subscribe((response) => {
       this._router.navigate(['/']);
     });
   }
